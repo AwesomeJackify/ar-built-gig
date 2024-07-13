1c4e9e3c5f1a071f2438f72438b8463c7b5ffe42
Jack Gong <jackgong48@gmail.com>
2024-07-14 11:26:35 +1200

    ADDED: cdn videos

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,30 @@
 import config from "../config.json";
 import { Icon } from "@iconify-icon/react";
-
-import project1Video from "../assets/videos/project1.mp4";
-import project2Video from "../assets/videos/project2.mp4";
-
 import { useEffect, useRef, useState } from "react";
 
-const Hero = () => {
-  const [heroVideo, setHeroVideo] = useState(project1Video);
-  const [checkItOutLink, setCheckItOutLink] = useState("/projects/project-1");
-  const [heroTitle, setHeroTitle] = useState("Project 1");
-  const [heroDescription, setHeroDescription] = useState(
-    "Residential and commercial spaces in tune with nature"
-  );
-  const heroVideo1Ref = useRef<HTMLVideoElement | null>(null);
-  const heroVideo2Ref = useRef<HTMLVideoElement | null>(null);
-  const miniVideo1Ref = useRef<HTMLVideoElement | null>(null);
-  const miniVideo2Ref = useRef<HTMLVideoElement | null>(null);
+interface VideoProject {
+  url: string;
+  title: string;
+  description: string;
+  link: string;
+}
 
+interface Props {
+  projects: VideoProject[];
+}
+
+const Hero = ({ projects }: Props) => {
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
   const [progress, setProgress] = useState(0);
+  const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
+  const miniVideoRefs = useRef<(HTMLVideoElement | null)[]>([]);
   const animationFrameId = useRef<number | null>(null);
 
-  useEffect(() => {
-    const video =
-      heroVideo === project1Video
-        ? heroVideo1Ref.current
-        : heroVideo2Ref.current;
+  const currentProject = projects[currentProjectIndex];
 
-    const miniVideo =
-      heroVideo === project1Video
-        ? miniVideo1Ref.current
-        : miniVideo2Ref.current;
+  useEffect(() => {
+    const video = videoRefs.current[currentProjectIndex];
+    const miniVideo = miniVideoRefs.current[currentProjectIndex];
 
     if (video && miniVideo) {
       setProgress(0);
@@ -54,13 +48,8 @@ const Hero = () => {
     };
 
     const handleEnded = () => {
-      if (heroVideo === project1Video) {
-        setHeroVideo(project2Video);
-        setProgress(0);
-      } else {
-        setHeroVideo(project1Video);
-        setProgress(0);
-      }
+      setCurrentProjectIndex((prevIndex) => (prevIndex + 1) % projects.length);
+      setProgress(0);
     };
 
     video?.addEventListener("timeupdate", handleTimeUpdate);
@@ -74,24 +63,11 @@ const Hero = () => {
         animationFrameId.current = null;
       }
     };
-  }, [heroVideo]);
+  }, [currentProjectIndex, projects.length]);
 
-  const handleProgressClick = (num: number) => {
-    if (num === 1 && heroVideo !== project1Video) {
-      setHeroVideo(project1Video);
-      setCheckItOutLink("/projects/project-1");
-      setHeroTitle("Project 1");
-      setHeroDescription(
-        "Residential and commercial spaces in tune with nature"
-      );
-    } else if (num === 2 && heroVideo !== project2Video) {
-      setHeroVideo(project2Video);
-      setCheckItOutLink("/projects/curved-deck");
-      setHeroTitle("Curved Deck");
-      setHeroDescription(
-        "A beautiful curved deck that complements the natural landscape"
-      );
-    }
+  const handleProgressClick = (index: number) => {
+    setCurrentProjectIndex(index);
+    setProgress(0);
   };
 
   return (
@@ -113,52 +89,40 @@ const Hero = () => {
         </div>
       </div>
       <div className="flex-1 relative max-md:h-96 max-md:hidden">
-        <video
-          ref={heroVideo1Ref}
-          autoPlay
-          muted
-          className={`${
-            heroVideo == project1Video ? "" : "hidden"
-          } w-full h-full object-cover max-md:hidden absolute top-0 left-0 right-0 bottom-0 grayscale-[50%]`}
-        >
-          <source src={project1Video} type="video/mp4" />
-        </video>
-        <video
-          ref={heroVideo2Ref}
-          autoPlay
-          muted
-          className={`${
-            heroVideo == project2Video ? "" : "hidden"
-          } w-full h-full object-cover max-md:hidden absolute top-0 left-0 right-0 bottom-0 grayscale-[50%]`}
-        >
-          <source src={project2Video} type="video/mp4" />
-        </video>
+        {projects.map((project, index) => (
+          <video
+            key={index}
+            ref={(el) => (videoRefs.current[index] = el)}
+            autoPlay
+            muted
+            className={`${
+              currentProjectIndex === index ? "" : "hidden"
+            } w-full h-full object-cover max-md:hidden absolute top-0 left-0 right-0 bottom-0 grayscale-[50%]`}
+          >
+            <source src={project.url} type="video/mp4" />
+          </video>
+        ))}
       </div>
-      <div className="md:absolute max-md:w-11/12 max-md:mx-auto w-3/12 top-1/2 left-1/2 -translate-x-1/4 -translate-y-1/2 bg-primary p-4 flex flex-col items-center gap-8 pb-12 max-md:-translate-x-0 max-md:-translate-y-0 max-md:mb-8">
+      <div className="md:absolute max-md:w-11/12 max-md:mx-auto w-3/12 top-1/2 left-1/2 -translate-x-1/4 -translate-y-1/2 bg-primary p-4 flex flex-col items-center gap-8 pb-12 max-md:-translate-x-0 max-md:-translate-y-0 max-md:mb-8 max-md:hidden">
         <div className="relative">
-          <video
-            ref={miniVideo1Ref}
-            autoPlay
-            muted
-            className={`${
-              heroVideo == project1Video ? "" : "hidden"
-            } w-full h-full object-cover aspect-square`}
-          >
-            <source src={project1Video} type="video/mp4" />
-          </video>
-          <video
-            ref={miniVideo2Ref}
-            autoPlay
-            muted
-            className={`${
-              heroVideo == project2Video ? "" : "hidden"
-            } w-full h-full object-cover aspect-square`}
-          >
-            <source src={project2Video} type="video/mp4" />
-          </video>
+          {projects.map((project, index) => (
+            <video
+              key={index}
+              ref={(el) => (miniVideoRefs.current[index] = el)}
+              autoPlay
+              muted
+              playsInline
+              webkit-playsinline="true"
+              className={`${
+                currentProjectIndex === index ? "" : "hidden"
+              } w-full h-full object-cover aspect-square`}
+            >
+              <source src={project.url} type="video/mp4" />
+            </video>
+          ))}
           <div className="absolute bottom-4 w-full flex justify-center">
             <a
-              href={checkItOutLink}
+              href={currentProject.link}
               className="btn flex items-center gap-1 bg-gray-200 p-4 py-3 w-fit"
             >
               <Icon icon="fluent:tap-double-20-filled" className="text-2xl" />
@@ -168,33 +132,28 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col items-center text-center gap-2 text-white">
-          <h1 className="text-5xl">{heroTitle}</h1>
-          <h2 className="font-extralight text-xl">{heroDescription}</h2>
+          <h1 className="text-5xl">{currentProject.title}</h1>
+          <h2 className="font-extralight text-xl">
+            {currentProject.description}
+          </h2>
         </div>
         <div className="flex w-full gap-2 h-[2px]">
-          <div className="relative w-full h-full flex items-center">
+          {projects.map((_, index) => (
             <div
-              onClick={() => handleProgressClick(1)}
-              className="h-4 top-0 -translate-y-1/2 left-0 w-full absolute z-10 cursor-pointer"
-            ></div>
-            <progress
-              className="cursor-pointer progress w-full h-full bg-white/20 [&::-webkit-progress-value]:bg-white"
-              value={heroVideo == project1Video ? progress : 0}
-              max="100"
-            ></progress>
-          </div>
-
-          <div className="relative w-full h-full flex items-center">
-            <div
-              onClick={() => handleProgressClick(2)}
-              className="h-4 top-0 -translate-y-1/2 left-0 w-full absolute z-10 cursor-pointer"
-            ></div>
-            <progress
-              className="cursor-pointer progress w-full h-full bg-white/20 [&::-webkit-progress-value]:bg-white"
-              value={heroVideo == project2Video ? progress : 0}
-              max="100"
-            ></progress>
-          </div>
+              key={index}
+              className="relative w-full h-full flex items-center"
+            >
+              <div
+                onClick={() => handleProgressClick(index)}
+                className="h-4 top-0 -translate-y-1/2 left-0 w-full absolute z-10 cursor-pointer"
+              ></div>
+              <progress
+                className="cursor-pointer progress w-full h-full bg-white/20 [&::-webkit-progress-value]:bg-white"
+                value={currentProjectIndex === index ? progress : 0}
+                max="100"
+              ></progress>
+            </div>
+          ))}
         </div>
       </div>
     </div>
